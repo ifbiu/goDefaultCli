ba7fd3da9c7f1677376acb6cb858ddd661ea1bae
candide <biuvip@163.com>
2021-08-21 00:47:58 +0800

    初始化日志

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module goDeaultCli
 go 1.16
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gin-gonic/gin v1.7.4
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/viper v1.8.1
+	go.uber.org/zap v1.17.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"go.uber.org/zap"
+	"goDeaultCli/logger"
 	"goDeaultCli/settings"
 )
 
@@ -12,6 +14,11 @@ func main() {
 		return
 	}
 	// 2. 初始化日志
+	if err := logger.Init(); err != nil {
+		fmt.Printf("init logger failed! err: %v\n", err)
+		return
+	}
+	zap.L().Debug("logger init success...")
 	// 3. 初始化MySQL连接
 	// 4. 初始化Redis连接
 	// 5. 注册路由
